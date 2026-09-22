fda7a289528685eb32456b5f239613af32383330
huu144 <huu144@gmail.com>
2026-09-22 11:46:04 +0700

    Fix Prisma pooler prepared statements

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 
+if (process.env.DATABASE_URL?.includes('pooler')) {
+  const databaseUrl = new URL(process.env.DATABASE_URL)
+  databaseUrl.searchParams.set('pgbouncer', 'true')
+  databaseUrl.searchParams.set('connection_limit', '1')
+  process.env.DATABASE_URL = databaseUrl.toString()
+}
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
